fbf7c8b92dc76e6ea5d9a9445fbf2cf242293de4
Weverton do Couto Timoteo <weverton@wolcan.us>
2014-02-10 23:38:29 -0200

    Space Escape view

---
+++ Space Escape/SpaceEscape.h
@@ -0,0 +1,13 @@
+//
+//  SpaceEscape.h
+//  Space Escape
+//
+//  Created by Weverton Couto Timoteo on 2/10/14.
+//  Copyright (c) 2014 Weverton Couto Timoteo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpaceEscape : UIView
+
+@end
---
+++ Space Escape/SpaceEscape.m
@@ -0,0 +1,22 @@
+//
+//  SpaceEscape.m
+//  Space Escape
+//
+//  Created by Weverton Couto Timoteo on 2/10/14.
+//  Copyright (c) 2014 Weverton Couto Timoteo. All rights reserved.
+//
+
+#import "SpaceEscape.h"
+
+@implementation SpaceEscape
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+
+    }
+    return self;
+}
+
+@end
